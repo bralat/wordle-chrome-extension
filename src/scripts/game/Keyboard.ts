92ab3c6cbb58502942d882eb2134978539598a24
bralat <11743617+bralat@users.noreply.github.com>
2023-05-14 20:54:21 +0100

    Board and Keyboard classes made static

--- src/scripts/game/Keyboard.ts
+++ src/scripts/game/Keyboard.ts
@@ -1,47 +1,65 @@
+import { LetterState } from "../types/LetterState"
+import { LetterStatePosition } from "../types/LetterStatePosition"
+
 export default class Keyboard
 {
   static BACKSPACE_KEY: string = '←'
   static ENTER_KEY: string = '↵'
 
-  element: HTMLElement
+  static element: HTMLElement
   static keyboardClass: string = '.Keyboard-module_keyboard__uYuqf'
-  letters: {
-    [x: string]: String[],
-  }
+  static _letters: LetterStatePosition = {}
   
   // states
-  readonly EMPTY_STATE: string = 'empty'
-  readonly ABSENT_STATE: string = 'absent'
-  readonly PRESENT_STATE: string = 'present'
-  readonly CORRECT_STATE: string = 'correct'
+  static readonly EMPTY_STATE: string = 'empty'
+  static readonly ABSENT_STATE: string = 'absent'
+  static readonly PRESENT_STATE: string = 'present'
+  static readonly CORRECT_STATE: string = 'correct'
 
-  constructor() {
-    this.letters = {
-      [this.EMPTY_STATE]: [],
-      [this.ABSENT_STATE]: [],
-      [this.PRESENT_STATE]: [],
-      [this.CORRECT_STATE]: [],
-    }
-    // this.categoriseLetters()
-  }
-
-  categoriseLetters () {
-    this.element = document.querySelector(Keyboard.keyboardClass) as HTMLElement;
-    this.element.querySelectorAll('button.Key-module_key__kchQI').forEach((elem: Element) => {
-      const state: string = elem.getAttribute('data-state') as string;
-      if (state) {
-        this.letters[state].push(elem.getAttribute('data-key') as string);
-      } else {
-        this.letters[this.EMPTY_STATE].push(elem.getAttribute('data-key') as string);
+  static categoriseLetters () {
+    Keyboard.element = document.querySelector(Keyboard.keyboardClass) as HTMLElement;
+    console.log(Keyboard.element.querySelectorAll('button.Key-module_key__kchQI'));
+    Keyboard.element.querySelectorAll('button.Key-module_key__kchQI').forEach((elem: Element) => {
+      const state: LetterState = elem.getAttribute('data-state') as LetterState;
+      const letter: string = elem.getAttribute('data-key') as string;
+      if (Keyboard.alphabet.includes(letter)) {
+        Keyboard._letters[letter] = {
+          state: state,
+          position: null
+        }
       }
+      // const state: string = elem.getAttribute('data-state') as string;
+      // if (state) {
+      //   Keyboard.letters[state].push(elem.getAttribute('data-key') as string);
+      // } else {
+      //   Keyboard.letters[Keyboard.EMPTY_STATE].push(elem.getAttribute('data-key') as string);
+      // }
     })
   }
 
-  allLettersFound (): Boolean {
-    return this.letters[this.CORRECT_STATE].length === 5;
-  }
+  // static allLettersFound (): Boolean {
+  //   return Keyboard.letters[Keyboard.CORRECT_STATE].length === 5;
+  // }
 
   static hit (key: string) {
     document.querySelector(`${this.keyboardClass} button[data-key='${key}']`)?.click();
+  }
+
+  static get letters () {
+    Keyboard.categoriseLetters()
+
+    return Keyboard._letters;
+  }
+
+  static enter () {
+    Keyboard.hit(Keyboard.ENTER_KEY);
+  }
+
+  static backspace () {
+    Keyboard.backspace();
+  }
+
+  static get alphabet(): string[] {
+    return ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   }
 }
