1f511d9900390046a85326ecf39f59921c064d06
Abdul-Latif Amadu <abdul@wearedomo.com>
2023-08-31 20:59:54 +0100

    foreach and hide directives added: WIP

---
+++ src/scripts/directives/Reactive.ts
@@ -0,0 +1,23 @@
+export class Reactive<T>
+{
+    _value: T;
+    _effect?: (value: any) => void
+
+    constructor (value: T, effect?: () => void) {
+        this._value = value;
+        this._effect = effect;
+    }
+
+    setEffect(effect: (value: T) => void) {
+        this._effect = effect;
+    }
+
+    get value(): T {
+        return this._value;
+    }
+
+    set value(value: T) {
+        this._effect && this._effect(value)
+        this._value = value;
+    }
+}
--- src/scripts/elements/BaseElement.ts
+++ src/scripts/elements/BaseElement.ts
@@ -1,16 +1,64 @@
+import Directive from "../directives/Directive";
+
 export default class BaseElement extends HTMLElement
 {
   shadow: ShadowRoot;
+  eventHandlers: {
+    [eventName: string]: (e: Event) => void
+  }
 
   constructor () {
     super()
     this.shadow = this.attachShadow({mode: 'open'});
   }
 
-  createElementFromString(html: string): HTMLCollection {
+  createElementFromString(html: string): HTMLCollection|Element {
     const div: HTMLDivElement = document.createElement('div') as HTMLDivElement
     div.innerHTML = html.trim();
 
-    return div.children;
+    return div.children.length === 1 ? div.children[0] : div.children;
+  }
+
+  setStyle() {
+    const styleElem = document.createElement('style')
+    styleElem.innerHTML = this.css
+
+    this.shadow.appendChild(styleElem)
+  }
+
+  render() {
+    this.setStyle()
+    this.pipeline(this.view, [
+      this.createElementFromString,
+      this.runDirectives.bind(this),
+      this.createEventListeners,
+      this.appendToView.bind(this),
+    ])
+    
+  }
+
+  runDirectives(element: Element): Element {
+    console.log('con', this)
+    return (new Directive(element, this)).finalElement
+  }
+
+  createEventListeners(element: Element): Element {
+    return element;
+  }
+
+  appendToView(element: HTMLElement): Element {
+    this.shadow.appendChild(element)
+    console.log(element);
+
+    return element;
+  }
+
+  pipeline(element: HTMLElement, handlers: ((...args: any[]) => any)[]): HTMLElement {
+    let result = element;
+    handlers.forEach((handler: (...args: any[]) => any) => {
+      result = handler(result)
+    })
+
+    return result
   }
 }
