76fac62a307776c06e3503b2d47d61b2ba308b55
Abdul-Latif Amadu <abdul@wearedomo.com>
2023-10-16 21:57:03 +0100

    fixtures used to simplify test

--- tests/unit/App.test.js
+++ tests/unit/App.test.js
@@ -1,7 +1,15 @@
-import { jest, describe, expect, test } from '@jest/globals';
+import { describe, expect, test, fixture } from '@jest/globals';
 import App from "../../src/scripts/App";
+import Board from '../../src/scripts/game/Board';
+import mockView from '../fixtures/mockView.js';
+import mockDictionary from '../fixtures/mockDictionary';
+import Predictor from '../../src/scripts/predictor/Predictor';
 
 describe('App.ts', () => {
+    beforeAll(() => {
+        jest.useFakeTimers();
+    });
+
     test('it should get dictionary from storage if already loaded', () => {
         // Given
         global.fetch = jest.fn(() =>
@@ -21,7 +29,7 @@ describe('App.ts', () => {
         // Then
         // assert no http request was made
         expect(global.fetch).not.toHaveBeenCalled()
-    })
+    });
 
     test('it should get dictionary via http request', () => {
         // Given
@@ -47,22 +55,41 @@ describe('App.ts', () => {
         expect(global.fetch).toHaveBeenCalled()
     })
 
-    test('it should wait 2 seconds after script is executed to declare it\'s ready', () => {
-
-    })
-
-    test('it should initialise event listeners', () => {
-
-    })
     test('it should update predictions when reset', () => {
+        // Given
+        document.body.innerHTML = mockView;
+        localStorage.setItem('words', JSON.stringify(mockDictionary));
+        const board = new Board({
+            'board': '.Board-module_boardContainer__TBHNL',
+            'row': '.Row-module_row__pwpBq',
+            'column': 'div.Tile-module_tile__UWEHN'
+        });
+        const app = new App(board);
+        const currentWords = app.wordSelector.element._words.value;
 
-    })
+        // When
+        app.reset()
+        jest.advanceTimersByTime(3000)
 
-    test('it should run predictions if game is in progress', () => {
-
+        // Then
+        expect(app.wordSelector.element._words.value).not.toBe(currentWords);
     })
 
     test('it should use starter words if it\'s a new game', () => {
+        document.body.innerHTML = mockView;
+        localStorage.setItem('words', JSON.stringify(mockDictionary));
+        const board = new Board({
+            'board': '.Board-module_boardContainer__TBHNL',
+            'row': '.Row-module_row__pwpBq',
+            'column': 'div.Tile-module_tile__UWEHN'
+        });
+        const app = new App(board);
 
+        // When
+        app.initExtension()
+        jest.advanceTimersByTime(3000)
+
+        // Then
+        expect(app.wordSelector.element._words.value).toBe(Predictor.starterWords);
     })
 })
