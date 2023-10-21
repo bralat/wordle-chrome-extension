b2c95123f62d9fa2a371304c294c98055034a322
bralat <11743617+bralat@users.noreply.github.com>
2023-10-21 15:19:23 +0100

    letter rule test coverage increased

--- tests/unit/predictor/LetterRule.spec.js
+++ tests/unit/predictor/LetterRule.spec.js
@@ -1,25 +1,49 @@
 import LetterRule from "@/src/scripts/predictor/LetterRule"
 
 describe('LetterRule', () => {
-    it('it satisfies rule if letter matches', () => {
+    it('satisfies "is" rule if letter matches', () => {
         // Given
         const letterRule = new LetterRule();
         letterRule.is('A');
 
         // When
         const doesItMatch = letterRule.satisfies('A');
-        
+
         // Then
         expect(doesItMatch).toBe(true);
     });
 
-    it('it satisfies rule if letter does not match', () => {
+    it('does not satisfy "is" rule if letter does not match', () => {
         // Given
         const letterRule = new LetterRule();
         letterRule.is('A');
 
         // When
         const doesItMatch = letterRule.satisfies('B');
+
+        // Then
+        expect(doesItMatch).toBe(false);
+    });
+
+    it('it satisfies "isNot" rule if letter does not match', () => {
+        // Given
+        const letterRule = new LetterRule();
+        letterRule.isNot('A');
+
+        // When
+        const doesItMatch = letterRule.satisfies('B');
+        
+        // Then
+        expect(doesItMatch).toBe(true);
+    });
+
+    it('it does not satisfy "isNot" rule if letter matches', () => {
+        // Given
+        const letterRule = new LetterRule();
+        letterRule.isNot('A');
+
+        // When
+        const doesItMatch = letterRule.satisfies('A');
         
         // Then
         expect(doesItMatch).toBe(false);
