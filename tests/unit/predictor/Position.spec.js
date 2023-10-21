c105e3d060d1cd884fd335bca6546589ba17cb4b
bralat <11743617+bralat@users.noreply.github.com>
2023-10-21 15:50:44 +0100

    Position tests added

---
+++ tests/unit/predictor/Position.spec.js
@@ -0,0 +1,35 @@
+import Position from '@/src/scripts/predictor/Position'
+import LetterRule from "@/src/scripts/predictor/LetterRule"
+
+let position;
+describe ('Predictor/Position', () => {
+    beforeEach(() => {
+        position = new Position();
+        ['A', 'C'].forEach((letter) => {
+            const letterRule = new LetterRule();
+            letterRule.isNot(letter);
+            position.addRule(letterRule);
+        });
+    });
+
+    it('adds multiple rules per position', () => {
+        // When/Then
+        expect(position.rules.length).toBe(2)
+    });
+
+    it('satisfies all rules', () => {
+        // When
+        const doesItMatch = position.satisfies('B')
+
+        // Then
+        expect(doesItMatch).toBe(true);
+    });
+
+    it('fails if one rule is not satisfied', () => {
+        // When
+        const doesItMatch = position.satisfies('C')
+
+        // Then
+        expect(doesItMatch).toBe(false);
+    });
+});
