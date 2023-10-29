ba61611de366dc382255acc138686c5a1d35bd71
Abdul-Latif Amadu <abdul@wearedomo.com>
2023-10-29 12:53:23 +0000

    Letter class test coverage increased
    existing tests refactored

--- tests/unit/game/Letter.spec.js
+++ tests/unit/game/Letter.spec.js
@@ -1,14 +1,17 @@
 import Letter from "@/src/scripts/game/Letter";
 
 describe('Game/Letter', () => {
-    it('extracts the state and letter from element', () => {
-        // Given
-        const element = document.createElement('div');
+    let element;
+
+    beforeEach(() => {
+        element = document.createElement('div');
         element.innerHTML = `
             <div data-state="present" data-key="a">a</div>
         `;
+    });
 
-        // When
+    it('extracts the state and letter from element', () => {
+        // Given/When
         const letter = new Letter(element.firstElementChild);
 
         // Then
@@ -17,57 +20,85 @@ describe('Game/Letter', () => {
     });
 
     it('checks the state of the letter', () => {
-        // Given
-        const element = document.createElement('div');
-        element.innerHTML = `
-            <div data-state="present" data-key="a">a</div>
-        `;
-
-        // When/Then
+        // Given/When
         const letter = new Letter(element.firstElementChild);
+
+        // Then
         expect(letter.isState('empty')).toBe(false);
         expect(letter.isState('present')).toBe(true);
     });
 
     it('detects states that have lower priority than itself', () => {
-        // Given
-        const element = document.createElement('div');
-        element.innerHTML = `
-            <div data-state="present" data-key="a">a</div>
-        `;
-
-        // When/Then
+        // Given/When
         const letter = new Letter(element.firstElementChild);
+
+        // Then
         expect(letter.isPriorityLowerThan('correct')).toBe(true);
         expect(letter.isPriorityLowerThan('present')).toBe(false);
         expect(letter.isPriorityLowerThan('absent')).toBe(false);
     });
 
     it('detects states that have have the same priority as itself', () => {
-        // Given
-        const element = document.createElement('div');
-        element.innerHTML = `
-            <div data-state="present" data-key="a">a</div>
-        `;
-
-        // When/Then
+        // Given/When
         const letter = new Letter(element.firstElementChild);
+
+        // Then
         expect(letter.isPriorityEqualTo('correct')).toBe(false);
         expect(letter.isPriorityEqualTo('present')).toBe(true);
         expect(letter.isPriorityEqualTo('absent')).toBe(false);
     });
 
     it('triggers the click event on the element', () => {
-        // Given
-        const element = document.createElement('div');
-        element.innerHTML = `
-            <div data-state="present" data-key="a">a</div>
-        `;
-
-        // When/Then
+        // Given/When
         const letter = new Letter(element.firstElementChild);
-        expect(letter.isPriorityEqualTo('correct')).toBe(false);
-        expect(letter.isPriorityEqualTo('present')).toBe(true);
-        expect(letter.isPriorityEqualTo('absent')).toBe(false);
+        const spy = jest.spyOn(element.firstElementChild, 'click');
+        letter.click();
+
+        // Then
+        expect(spy).toHaveBeenCalled();
     });
+
+    it('does not append state if state is not valid', () => {
+        // Given/When
+        const letter = new Letter(element.firstElementChild);
+        letter.appendState('invalid');
+
+        // Then
+        expect(letter.statePosition.state).toBe('present');
+    });
+
+    it('replaces current state if incoming state is higher', () => {
+        // Given/When
+        const letter = new Letter(element.firstElementChild);
+        letter.appendState('correct', 2);
+
+        // Then
+        expect(letter.statePosition.state).toBe('correct');
+        expect(letter.statePosition.positions).toStrictEqual([2]);
+    });
+
+    it('appends position if incoming state is equal to current state', () => {
+        // Given/When
+        const letter = new Letter(element.firstElementChild);
+        letter.appendState('present', 2);
+        letter.appendState('present', 3);
+
+        // Then
+        expect(letter.statePosition.state).toBe('present');
+        expect(letter.statePosition.positions).toStrictEqual([2, 3]);
+    });
+
+    it('adds listeners for click event', () => {
+        // Given
+        const letter = new Letter(element.firstElementChild);
+        const documentSpy = jest.spyOn(document, 'addEventListener');
+        const elementSpy = jest.spyOn(letter.element, 'addEventListener');
+
+        // When
+        letter.onClick(() => {});
+
+        // Then
+        expect(documentSpy).toHaveBeenCalledWith('keyup', expect.any(Function));
+        expect(elementSpy).toHaveBeenCalledWith('click', expect.any(Function));
+    })
 });
