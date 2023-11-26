51e691537a46ee01c2e03d69a33ec2c55c10b64a
bralat <11743617+bralat@users.noreply.github.com>
2023-11-26 21:09:54 +0000

    Predictor unit tests added

--- tests/unit/predictor/Predictor.spec.js
+++ tests/unit/predictor/Predictor.spec.js
@@ -10,16 +10,18 @@ jest.mock('@/scripts/predictor/WordRule')
 jest.mock('@/scripts/predictor/Position')
 
 function setKeyboardLetters(data) {
-    Keyboard._letters = data.map(letterData => {
-        const element = document.createElement('div');
-        element.setAttribute('data-state', letterData.statePosition.state);
-        element.setAttribute('data-key', letterData.letter);
+    jest.spyOn(Keyboard, 'letters', 'get').mockReturnValue(
+        data.map(letterData => {
+            const element = document.createElement('div');
+            element.setAttribute('data-state', letterData.statePosition.state);
+            element.setAttribute('data-key', letterData.letter);
 
-        const letter = new Letter(element);
-        letter.statePosition = letterData.statePosition;
+            const letter = new Letter(element);
+            letter.statePosition = letterData.statePosition;
 
-        return letter;
-    });
+            return letter;
+        })
+    );
 };
 
 describe('Predictor/Predictor', () => {
@@ -132,5 +134,153 @@ describe('Predictor/Predictor', () => {
         expect(WordRule.mock.instances[0].mustNotHave).toHaveBeenCalledWith('a');
         // assert the right number of word rules were generated
         expect(predictor.rules.length).toBe(1);
+    });
+
+    it('should get dictionary from storage if not already loaded', () => {
+        // Given
+        Predictor._dictionary = [
+            { word: 'word' }
+        ];
+        jest.spyOn(Storage.prototype, 'getItem');
+
+        // When
+        const dictionary = Predictor.dictionary
+
+        // Then
+        expect(global.localStorage.getItem).not.toHaveBeenCalled()
+        delete Predictor._dictionary;
+    });
+
+    it('should not get dictionary from storage if already loaded', () => {
+        // Given
+        global.localStorage.getItem = jest.fn(() => {
+            return JSON.stringify([
+                { word: 'word' }
+            ])
+        })
+        jest.spyOn(Storage.prototype, 'getItem');
+
+        // When
+        const dictionary = Predictor.dictionary
+
+        // Then
+        expect(global.localStorage.getItem).toHaveBeenCalled()
+    });
+
+    it('adds a word rule to the predictor', () => {
+        // Given
+        jest.spyOn(Predictor.prototype, 'setRules').mockImplementation(() => ({}));
+        const predictor = new Predictor();
+        const wordRule = new WordRule();
+
+        // When
+        predictor.addRule(wordRule);
+
+        // Then
+        expect(predictor.rules.length).toBe(1);
+        expect(predictor.rules[0]).toBe(wordRule);
+    });
+
+    it('checks all word rules', () => {
+        // Given
+        jest.spyOn(Predictor.prototype, 'setRules').mockImplementation(() => ({}));
+        const predictor = new Predictor();
+        predictor.rules = [
+            new WordRule(),
+            new WordRule(),
+        ];
+        predictor.rules[0].mustHave('a');
+        predictor.rules[1].mustNotHave('b');
+        // expect first rule to return value to second value is checked
+        predictor.rules[0].satisfies.mockReturnValue(true);
+
+        // When
+        predictor.satisfies('aycde');
+
+        // Then
+        expect(predictor.rules[0].satisfies).toHaveBeenCalledTimes(1);
+        expect(predictor.rules[1].satisfies).toHaveBeenCalledTimes(1);
+    });
+
+    it('checks if letter rules are a match', () => {
+        // Given
+        jest.spyOn(Predictor.prototype, 'setRules').mockImplementation(() => ({}));
+        const predictor = new Predictor();
+        predictor.positions = [
+            new Position(),
+        ];
+        predictor.positions[0].rules = [
+            new LetterRule(),
+        ];
+
+        // When
+        predictor.matchLetters('abcde');
+
+        // Then
+        expect(predictor.positions[0].satisfies).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not predict word if it does not match letter rule', () => {
+        // Given
+        jest.spyOn(Predictor.prototype, 'setRules').mockImplementation(() => ({}));
+        jest.spyOn(Predictor, 'dictionary', 'get').mockImplementation(() => [
+            { word: 'word' }
+        ]);
+        const predictor = new Predictor();
+        predictor.rules = [
+            new WordRule(),
+        ];
+        jest.spyOn(predictor, 'matchLetters');
+
+        // When
+        const words = predictor.predict();
+
+        // Then
+        expect(words.length).toBe(0);
+        expect(predictor.matchLetters).toHaveBeenCalledTimes(1);
+        expect(predictor.rules[0].satisfies).not.toHaveBeenCalled();
+    });
+
+    it('does not predict word if it does not match word rule', () => {
+        // Given
+        jest.spyOn(Predictor.prototype, 'setRules').mockImplementation(() => ({}));
+        jest.spyOn(Predictor, 'dictionary', 'get').mockImplementation(() => [
+            { word: 'word' }
+        ]);
+        const predictor = new Predictor();
+        predictor.rules = [
+            new WordRule(),
+        ];
+        jest.spyOn(predictor, 'matchLetters').mockImplementation(() => true);
+
+        // When
+        const words = predictor.predict();
+
+        // Then
+        expect(words.length).toBe(0);
+        expect(predictor.matchLetters).toHaveBeenCalledTimes(1);
+        expect(predictor.rules[0].satisfies).toHaveBeenCalledTimes(1);
+    });
+
+    it('predicts word if both letter and word rules are satisfied', () => {
+        // Given
+        jest.spyOn(Predictor.prototype, 'setRules').mockImplementation(() => ({}));
+        jest.spyOn(Predictor, 'dictionary', 'get').mockImplementation(() => [
+            { word: 'word' }
+        ]);
+        const predictor = new Predictor();
+        predictor.rules = [
+            new WordRule(),
+        ];
+        jest.spyOn(predictor, 'matchLetters').mockImplementation(() => true);
+        predictor.rules[0].satisfies.mockReturnValue(true);
+
+        // When
+        const words = predictor.predict();
+
+        // Then
+        expect(words.length).toBe(1);
+        expect(predictor.matchLetters).toHaveBeenCalledTimes(1);
+        expect(predictor.rules[0].satisfies).toHaveBeenCalledTimes(1);
     });
 });
