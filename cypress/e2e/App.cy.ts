4291c72111e6c2fd1765c2a24778a45aaa77ecc4
Abdul-Latif Amadu <abdul@wearedomo.com>
2024-01-14 21:07:12 +0000

    basic cypress tests added

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,5 +1,32 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('App', () => {
+  it('attaches the extension to the first row when the game is new', () => {
+    cy.visit('http://localhost:8100');
+
+    cy.get('.Row-module_row__pwpBq').then((rows) => {
+      const rowBoundingClientRect = rows.get(0).getBoundingClientRect();
+      cy.get('start-button')
+        .should('have.css', 'left', `${rowBoundingClientRect.right + 10}px`)
+        .should('have.css', 'top', `${rowBoundingClientRect.top + 5}px`);
+
+      cy.get('word-selector')
+        .should('have.css', 'left', `${rowBoundingClientRect.right + 80}px`)
+        .should('have.css', 'top', `${rowBoundingClientRect.top + 5}px`);
+    });
+  });
+
+
+  it('attaches the extension to the empty row when the game is in progress', () => {
+    cy.visit('http://localhost:8100/in_progress.html');
+
+    // assert that the start button exists
+    cy.get('.Row-module_row__pwpBq').then((rows) => {
+      const rowBoundingClientRect = rows.get(1).getBoundingClientRect();
+      cy.get('start-button')
+        .should('have.css', 'left', `${rowBoundingClientRect.right + 10}px`)
+        .should('have.css', 'top', `${rowBoundingClientRect.top + 5}px`);
+
+      cy.get('word-selector')
+        .should('have.css', 'left', `${rowBoundingClientRect.right + 80}px`)
+        .should('have.css', 'top', `${rowBoundingClientRect.top + 5}px`);
+    });
+  });});
