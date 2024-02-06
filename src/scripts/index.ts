61c368e8d799821b965bca557516c8bd70ba332c
bralat <11743617+bralat@users.noreply.github.com>
2024-02-06 20:08:17 +0000

    index refactored

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -4,12 +4,21 @@ import Keyboard from "./game/Keyboard";
 import StartButtonElement from "./elements/StartButtonElement";
 import WordSelectorElement from "./elements/WordSelectorElement";
 
+// register custom elements
+customElements.define('start-button', StartButtonElement);
+customElements.define('word-selector', WordSelectorElement);
+customElements.define('wordle-predictor', App) 
+
 App.getDictionary()
 App.ready().then(() => {
-  // register custom elements
-  customElements.define('start-button', StartButtonElement);
-  customElements.define('word-selector', WordSelectorElement);
-  const app: App = new App();
+  document.body.appendChild(new App())
 
-  document.body.appendChild(app)
+  // create global css
+  const styleElem = document.createElement('style')
+  styleElem.innerHTML = `
+    wordle-predictor {
+      transition: all 0.3s ease-in-out;
+    }
+  `
+  document.head.appendChild(styleElem)
 })
